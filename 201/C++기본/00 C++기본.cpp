91e6bb6c6ce2a6228f3e1902637f488436b92cd3
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-03-15 12:16:12 +0900

    namespace로 축약

--- 201/C++기본/00 C++기본.cpp
+++ 201/C++기본/00 C++기본.cpp
@@ -1,11 +1,14 @@
-//std:: cout를 포함하고 있음
+//std:: cout를 포함하고있음
 #include <iostream>
+//std::를 생략(이거 안 쓰는 사람도 많음)
+using namespace std;
 
 
 int main(void)
 {
 	//std:: cout 객체로 문자열을 출력
-	std:: cout << "안녕 세상아\n";
+	//std::endl 객체로 개행 처리
+	std:: cout << "안녕 세상아" << std::endl;
 
 	return 0;
 }
