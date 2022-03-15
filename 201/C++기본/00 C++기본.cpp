9780ab02f4e4d6e86242dd77176950a81c8fcaf2
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-03-15 12:07:50 +0900

    printf를 std:: cout으로 대체 stdio.h를 iostream로 대체

--- 201/C++기본/00 C++기본.cpp
+++ 201/C++기본/00 C++기본.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
+//std:: cout를 포함하고 있음
+#include <iostream>
 
-int main(void) {
-	printf("안녕 세상아\n");
+
+int main(void)
+{
+	//std:: cout 객체로 문자열을 출력
+	std:: cout << "안녕 세상아\n";
 
 	return 0;
 }
