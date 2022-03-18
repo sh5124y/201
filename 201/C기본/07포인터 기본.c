900b7e34c95184525d7f9e825e184a7515caacae
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-03-18 09:24:13 +0900

    포인터 기본 2

---
+++ 201/C기본/07포인터 기본.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 10, num2 = 20, num3 = 30;
+
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+
+	//ptr1가 num2를 가리킴
+	ptr1 = ptr2;
+	//ptr2가 num3을 가리킴
+	ptr2 = ptr3;
+	
+	*ptr3 *= 2; //num3이90(45*2)
+	*ptr1 *= *ptr3; // num2 120(30+90)
+	*ptr2 *= 2; //num3 180(90+2)
+	printf(" %d, %d, %d", *ptr1, *ptr2, *ptr3);
+}
+
