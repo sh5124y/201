392ea277d724faa6b4e5ecc45cc05bc6f616b06b
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-03-25 09:18:29 +0900

    static을 안 쓸때

---
+++ 201/C기본/static 변수.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+void jungjuk(void) {
+	int a = 0;
+	cout << "지역변수 a는" << a << endl;
+	a++;
+}
+
+int main(void) {
+	jungjuk();
+	jungjuk();
+	jungjuk();
+}
