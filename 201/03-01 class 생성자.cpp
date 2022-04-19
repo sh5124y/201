4d9474ef5f67573cf4e5e374b7abe3c4d0c118f0
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-04-19 12:03:48 +0900

    멤버함수를 클래스 밖으로

--- 201/03-01 class 생성자.cpp
+++ 201/03-01 class 생성자.cpp
@@ -11,19 +11,21 @@ private :
 public:	
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다.
-	student() {
-		nHakbun =1234;
-		sName = "이사랑";
-		cout << "학번이 등록되었습니다." << endl;
+	student();
 
-	}
-	void show() {
-		cout << "학번은" << nHakbun<< "입니다" << endl;
-		cout << "이름은" << sName << "입니다" << endl;
-	}
+	void show();
 
 };
+student::student() {
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
 
+}
+void student :: show() {
+	cout << "학번은" << nHakbun << "입니다" << endl;
+	cout << "이름은" << sName << "입니다" << endl;
+}
 int main(void) {
 	student stu;
 	stu.show();
