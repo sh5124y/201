97bd2911cfb8489e6e86d023c950a15aed74f97d
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-04-19 11:55:10 +0900

    생성자

--- 201/03-01 class 기본생성자.cpp
+++ 201/03-01 class 기본생성자.cpp
@@ -3,17 +3,24 @@
 
 using namespace std;
 
-class Student {
+class student {
 private :
 	int nHakbun;
 	string sName;
-	//아래와같은 기본생성자를 컴파일러가 만들어줌
+
+public:	
+	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
+	//반환형을 갖고있지 않는다.
+	student() {
+		nHakbun =1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+
+	}
 
 };
 
 int main(void) {
-	//기본생성자
-	Student student1;
-	Student student2 = Student();
+	student stu;
 	
 }
