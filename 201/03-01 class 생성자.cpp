373e377c4fa723958d679261c11e241b640b73c8
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-04-19 12:13:28 +0900

    생성자 오버로딩

--- 201/03-01 class 생성자.cpp
+++ 201/03-01 class 생성자.cpp
@@ -12,6 +12,7 @@ public:
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다.
 	student();
+	student(int Hakbun, string Name);
 
 	void show();
 
@@ -22,11 +23,21 @@ student::student() {
 	cout << "학번이 등록되었습니다." << endl;
 
 }
+student::student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+}
+
 void student :: show() {
 	cout << "학번은" << nHakbun << "입니다" << endl;
-	cout << "이름은" << sName << "입니다" << endl;
+	cout << "이름은" << sName << "입니다" << endl << endl;
 }
 int main(void) {
-	student stu;
-	stu.show();
+	student stu1 = student();
+	stu1.show();
+
+	student stu2 = student(1111,"JWP");
+	stu2.show();
 }
