f5d7da2efb903122d07d98dbaf14f02aee03aefa
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2022-05-13 09:56:25 +0900

    객체 동적할당

--- 201/03-01 class 생성자.cpp
+++ 201/03-01 class 생성자.cpp
@@ -35,19 +35,11 @@ void student :: show() {
 	cout << "이름은" << sName << "입니다" << endl << endl;
 }
 int main(void) {
-	//student stu1 = student();
-	//stu1.show();
 
-	//student stu2 = student(1111,"JWP");
-	//stu2.show();
+	student* stu2 = new student(1111,"JWP");
+	stu2->show();
 
-	int* ptr2 = new int[4];
-	for (int i = 0; i < 4; i++) {
-		ptr2[i] = 10 + i;
-		cout << ptr2[i] << " ";
-	}
-
-	delete []ptr2;
+	delete stu2;
 
 	return 0;
 }
